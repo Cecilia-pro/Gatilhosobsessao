c4cfc7b096999f44e7f4c185c58e4314adec36d1
CECÍLIA GOUVEIA <ceciliagouveiapersonal@gmail.com>
2025-10-28 17:25:33 +0000

    tire os tres pixels e script utm que te mandei.  adicione esses: <!-- Me

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '873690944468585');
+            fbq('init', '1414511846525182');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -40,7 +40,7 @@ export default function RootLayout({
         {/* Utmify Pixel */}
         <Script id="utmify-pixel" strategy="afterInteractive">
           {`
-            window.pixelId = "673be41f2c13892a5a916bd4";
+            window.pixelId = "685cb2c823907d2b7babe978";
             var a = document.createElement("script");
             a.setAttribute("async", "");
             a.setAttribute("defer", "");
@@ -62,7 +62,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
-          src="https://www.facebook.com/tr?id=873690944468585&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=1414511846525182&ev=PageView&noscript=1"
           />
         </noscript>
         {children}
